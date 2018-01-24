9f284a7a18b485a30fe9870987a97fc43ed23dd8
Elena G <elena.guzbla@gmail.com>
2018-01-24 14:02:28 +0100

    main, activities, restclient added...

--- app/src/main/java/com/example/jsonacdat/MainActivity.java
+++ app/src/main/java/com/example/jsonacdat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jsonacdat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick_lanzarExs(View v)
+    {
+        Intent unIntent = null;
+
+        switch (v.getId())
+        {
+            case R.id.btn_Primitiva:
+                unIntent = new Intent(MainActivity.this, PrimitivaActivity.class);
+                break;
+            case R.id.btn_Contactos:
+                unIntent = new Intent(MainActivity.this, ContactosActivity.class);
+                break;
+            case R.id.btn_ContactosGSON:
+                unIntent = new Intent(MainActivity.this, ContactosGSONActivity.class);
+                break;
+            case R.id.btn_Creacion:
+                unIntent = new Intent(MainActivity.this, CreacionActivity.class);
+                break;
+            case R.id.btn_Retrofit:
+                unIntent = new Intent(MainActivity.this, RetrofitActivity.class);
+                break;
+        }
+
+        startActivity(unIntent);
     }
 }
